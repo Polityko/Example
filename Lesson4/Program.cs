618cfdcadf168694b355b82219dd964d02151c10
Olga <politykoolga@outlook.com>
2022-08-21 18:33:21 +0300

    Задача 4

---
+++ Lesson4/Program.cs
@@ -0,0 +1,15 @@
+﻿int N;   
+                      
+Console.WriteLine("введите число: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++) 
+{
+    
+    if (i%2==0)
+    {
+        if(i!=1)
+            Console.Write(",");
+        Console.Write(i);
+    }
+}   
