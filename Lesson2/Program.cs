f7be71d4a2b609f90577cdb003b2a23953efc494
Olga <politykoolga@outlook.com>
2022-08-21 18:30:00 +0300

    Задача 2

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 5;
+int numberB = 3;
+int numberC = 125;
+
+int max = numberA;
+
+if (numberA > max) {max = numberA;}
+if (numberB > max) {max = numberB;}
+if (numberC > max) {max = numberC;}
+
+Console.WriteLine (max);
