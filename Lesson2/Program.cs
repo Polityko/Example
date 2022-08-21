8d81fe9470f3f476b10b7d22916e968d6fce068a
Olga <politykoolga@outlook.com>
2022-08-21 18:47:07 +0300

    Redaction

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,11 +1,19 @@
-﻿int numberA = 5;
-int numberB = 3;
-int numberC = 125;
+﻿int a;
+Console.WriteLine("введите число: ");
+a = Convert.ToInt32(Console.ReadLine()); 
 
-int max = numberA;
+int b;
+Console.WriteLine("введите число: ");
+b = Convert.ToInt32(Console.ReadLine()); 
 
-if (numberA > max) {max = numberA;}
-if (numberB > max) {max = numberB;}
-if (numberC > max) {max = numberC;}
+int c;
+Console.WriteLine("введите число: ");
+c = Convert.ToInt32(Console.ReadLine()); 
 
-Console.WriteLine (max);
+int max = a;
+
+if (a > max) {max = a;}
+if (b > max) {max = b;}
+if (c > max) {max = c;}
+
+Console.WriteLine ("max = " + max);
